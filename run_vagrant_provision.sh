9367637be6bf240a3eb5bde5f1229bd19577a0c1
Peter Lomakin <plomakin@mirantis.com>
2013-10-14 13:26:22 +0400

    [ci] Added script for vagrant parallel provision of web and devstack

---
+++ run_vagrant_provision.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+ 
+MAX_PROCS=4
+ 
+parallel_provision() {
+    while read box; do
+        echo "Provisioning '$box'. Output will be in: $box.out.txt" 1>&2
+        echo $box
+    done | xargs -P $MAX_PROCS -I"BOXNAME" \
+        sh -c 'vagrant provision BOXNAME >BOXNAME.out.txt 2>&1 || echo "Error Occurred: BOXNAME"'
+}
+ 
+## -- main -- ##
+ 
+# start boxes sequentially to avoid vbox explosions
+vagrant up --no-provision
+ 
+# but run provision tasks in parallel
+cat <<EOF | parallel_provision
+web
+dev
+EOF
