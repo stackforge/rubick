2660aca28b68ee2578cfcb9763ae56990963cc36
Kirill Ishanov <kishanov@mirantis.com>
2013-10-21 02:57:39 -0700

    [ui] refactored addClusterForm validation to show errors properly in angular 1.2

--- config-validator-ui-concept/js/controllers/validate_controller.js
+++ config-validator-ui-concept/js/controllers/validate_controller.js
@@ -5,6 +5,7 @@ angular.module('rubick.controllers', []).
     controller('ValidateCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.currentStep = "cluster";
     $scope.ruleGroup = "valid";
+    $scope.ipPattern = "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
 
     $scope.setStep = function(step) {
         $scope.currentStep = step;
@@ -15,6 +16,33 @@ angular.module('rubick.controllers', []).
     }
 
     $('.ui.accordion').accordion();
+
+    $scope.getAddClusterFormErrors = function() {
+        var errors = []
+        var required = $scope.addClusterForm.$error.required;
+        var pattern = $scope.addClusterForm.$error.pattern;
+
+        if (required) {
+            _.each(required, function(e) {
+                switch (e.$name) {
+                    case 'name':
+                        errors.push("Cluster name cannot be empty.");
+                    break;
+                    case 'private_key':
+                        errors.push("SSH Key is missing.");
+                    break;
+                    default:
+                        break;
+                }
+            });
+        }
+
+        if (pattern) {
+            errors.push("Invalid IP address.");
+        }
+
+        return errors;
+    }
 
     $scope.showAddClusterModal = function() {
         $('.ui.modal').modal();
@@ -30,7 +58,6 @@ angular.module('rubick.controllers', []).
     });
 
     $scope.addCluster = function() {
-        console.log($scope.newCluster);
         $http.post('/clusters', $scope.newCluster).success(function() {
             $scope.clusters.push($scope.newCluster);
         });
