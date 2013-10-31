77795ede3ed029bba21ea80e76696ec72a3394d2
Maxim Kulkin <mkulkin@mirantis.com>
2013-10-31 17:26:15 +0400

    Fixed tests for ConfigSchemaRegistry (were not updated to new API)

--- rubick/test_config_schema_registry.py
+++ rubick/test_config_schema_registry.py
@@ -9,49 +9,40 @@ class ConfigSchemaRegistryTests(unittest.TestCase):
     def test_sample(self):
         nova = ConfigSchemaRegistry.register_schema(project='nova')
 
-        nova.version('1.0.0')
-        nova.section('DEFAULT')
-        nova.param(name='verbose', type='boolean')
-        nova.param(name='rabbit_host', type='address')
+        with nova.version('1.0.0', checkpoint=True) as cfg:
+            cfg.param(name='verbose', type='boolean')
+            cfg.param(name='rabbit_host', type='address')
 
-        nova.version('1.1.0')
-        nova.section('DEFAULT')
-        nova.param(name='verbose', type='boolean', default=False)
-        nova.remove_param('rabbit_host')
+        with nova.version('1.1.0') as cfg:
+            cfg.param(name='verbose', type='boolean', default=False)
+            cfg.remove_param('rabbit_host')
 
-        nova.commit()
-
-        schema10 = ConfigSchemaRegistry.get_schema(
-            project='nova', version='1.0.0')
+        schema10 = ConfigSchemaRegistry.get_schema(project='nova',
+                                                   version='1.0.0')
 
         self.assertEqual(Version('1.0.0'), schema10.version)
         self.assertEqual('ini', schema10.format)
 
-        verbose_param = find(
-            schema10.parameters,
-            lambda p: p.name == 'verbose')
+        def find_param(params, name):
+            return find(params, lambda p: p.name == name)
+
+        verbose_param = find_param(schema10.parameters, 'verbose')
         self.assertIsNotNone(verbose_param)
         self.assertEqual('boolean', verbose_param.type)
         self.assertEqual(None, verbose_param.default)
 
-        rabbit_host_param = find(
-            schema10.parameters,
-            lambda p: p.name == 'rabbit_host')
+        rabbit_host_param = find_param(schema10.parameters, 'rabbit_host')
         self.assertIsNotNone(rabbit_host_param)
         self.assertEqual('address', rabbit_host_param.type)
 
-        schema11 = ConfigSchemaRegistry.get_schema(
-            project='nova', version='1.1.0')
+        schema11 = ConfigSchemaRegistry.get_schema(project='nova',
+                                                   version='1.1.0')
 
-        verbose_param11 = find(
-            schema11.parameters,
-            lambda p: p.name == 'verbose')
+        verbose_param11 = find_param(schema11.parameters, 'verbose')
         self.assertIsNotNone(verbose_param11)
         self.assertEqual(False, verbose_param11.default)
 
-        rabbit_host_param11 = find(
-            schema11.parameters,
-            lambda p: p.name == 'rabbit_host')
+        rabbit_host_param11 = find_param(schema11.parameters, 'rabbit_host')
         self.assertIsNone(rabbit_host_param11)
 
 
