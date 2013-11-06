bbbcc21b12f47745f36dbd2b6d4b8b445e02cff7
Ryabin Sergey <sryabin@mirantis.com>
2013-11-06 14:38:53 +0400

    [rubick] advanced cluster list

--- rubick/cli.py
+++ rubick/cli.py
@@ -1,10 +1,13 @@
 import sys
 
+#temporary workaround for rubick/json and system json modules
 sys.path = sys.path[1:]
 
 import argparse
 import requests
 import json
+
+Api_url = None
 
 
 def arg_parse():
@@ -16,17 +19,31 @@ def arg_parse():
     return p.parse_args()
 
 
+def rubick_clusters_list():
+    global Api_url
+
+    r = requests.get("%s/clusters" % Api_url)
+    code = 1
+
+    if (r.status_code == requests.codes.ok):
+        code = 0
+        for cluster in json.loads(r.text):
+            print 'Cluster: %s' % cluster['name']
+            print '\tid: %s' % cluster['id']
+            print '\tnodes: %s' % len(cluster['nodes'])
+            print '\tdescription: %s' % cluster['description']
+
+    return code
+
+
 def main():
     args = arg_parse()
+    global Api_url
+
+    Api_url = args.api
 
     if (args.list_clusters):
-        r = requests.get("%s/clusters" % args.api)
-        if (r.status_code == requests.codes.ok):
-            print "Avialable clusters:"
-            for cluster in json.loads(r.text):
-                print "\t%s (%d nodes)" % (
-                    cluster['name'], len(cluster['nodes'])
-                    )
+        return rubick_clusters_list()
 
 
 if __name__ == '__main__':
